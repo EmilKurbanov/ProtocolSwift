a0ff12870572863042c76de64bd36637490ad77c
voitivit <kurbanov_emil@icloud.com>
2024-07-16 10:13:35 +0300

    final project added

--- ProtocolSwiftExample/ProtocolSwiftExample/ViewControllers/FirstVC.swift
+++ ProtocolSwiftExample/ProtocolSwiftExample/ViewControllers/FirstVC.swift
@@ -6,14 +6,63 @@
 //
 
 import UIKit
+import SnapKit
 
 class FirstVC: UIViewController {
-
+    private var sec: SecondVC!
+    var buttonExample: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Нажать", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.backgroundColor = .red
+        button.addTarget(self, action: #selector(actionbutton), for: .touchUpInside)
+        return button
+    }()
+    
+    let label: UILabel = {
+        let label = UILabel()
+        label.text = "Испорчено"
+        label.textAlignment = .center
+        return label
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = .white
+        sec = SecondVC(delegate: self)
+        settingsConstreints()
+        
+    }
+    
+    func settingsConstreints() {
+        view.addSubview(buttonExample)
+        view.addSubview(label)
+        
+        label.snp.makeConstraints { make in
+            make.top.equalTo(view.snp.top).offset(100)
+            make.left.equalTo(view.snp.left).offset(20)
+            make.right.equalTo(view.snp.right).offset(-20)
+            make.height.equalTo(50)
+            
+            
+        }
+        
+        buttonExample.snp.makeConstraints { make in
+            make.top.equalTo(label.snp.top).offset(100)
+            make.left.equalTo(view.snp.left).offset(20)
+            make.right.equalTo(view.snp.right).offset(-20)
+            make.height.equalTo(50)
+        }
     }
 
-
+    @objc func actionbutton() {
+        sec.textDest()
+    }
 }
 
+extension FirstVC: TextDelegate {
+    func textDestination(text: String) {
+        label.text = text
+    }
+    
+    
+}
--- ProtocolSwiftExample/ProtocolSwiftExample/ViewControllers/SecondVC.swift
+++ ProtocolSwiftExample/ProtocolSwiftExample/ViewControllers/SecondVC.swift
@@ -8,11 +8,24 @@
 import UIKit
 
 class SecondVC: UIViewController {
+ 
+    weak var delegate: TextDelegate!
 
+    init(delegate: TextDelegate!) {
+        self.delegate = delegate
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .gray
     }
 
-
+    func textDest(){
+        self.delegate.textDestination(text: "Починил")
+    }
 }
